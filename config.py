882069534cb7c7c36a59d98cee9b962fd67be28f
mkxrd <103378650+mkxrd@users.noreply.github.com>
2023-01-16 18:41:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ import tweepy
 تقدر تجيب مفاتيح لحسابك بتويتر من هنا
 developer.twitter.com 
 """    
-consumer_key = "xxxxxx"
-consumer_secret = "xxxxxx"
-access_token= "xxxxxx"
-access_secret = "xxxxxx"
+consumer_key = "n0JKPJt1wZ9omLIttqTtcMkZR"
+consumer_secret = "vIkuurPc7XqeL553RqVHr7wwsTC3ETeKxCXrR5DmSOn3mC2kmI"
+access_token= "1305148587010031617-QUPMmg7QInrSutTbejwGGXbgxRrkek"
+access_secret = "n2GfLIIWtH1hg282xV8nUsJoBnRBmvYyO4uUxqpzIEN2T"
 
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
